ff5af103b8ac6efbe0acb729f04a15dc26b346c4
Francisco Chalan <francischalan@gmail.com>
2025-04-20 19:23:02 -0500

    feat: end section 27

---
+++ curso-kubernetes/msvc-cursos/src/main/java/com/jchalan/springcloud/msvc/cursos/controllers/CursoController.java
@@ -0,0 +1,59 @@
+package com.jchalan.springcloud.msvc.cursos.controllers;
+
+import com.jchalan.springcloud.msvc.cursos.models.entity.Curso;
+import com.jchalan.springcloud.msvc.cursos.services.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CursoController {
+
+    @Autowired
+    private CursoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Curso>> listar() {
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id) {
+        Optional<Curso> curso = service.porId(id);
+        if (curso.isPresent()) {
+            return ResponseEntity.ok(curso.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> crear(@RequestBody Curso curso) {
+        Curso cursoDb = service.guardar(curso);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cursoDb);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@PathVariable Long id, @RequestBody Curso curso) {
+        Optional<Curso> o = service.porId(id);
+        if (o.isPresent()) {
+            Curso cursoDb = o.get();
+            cursoDb.setNombre(curso.getNombre());
+            return  ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(cursoDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        Optional<Curso> o = service.porId(id);
+        if (o.isPresent()) {
+            service.eliminar(o.get().getId());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ curso-kubernetes/msvc-cursos/src/main/java/com/jchalan/springcloud/msvc/cursos/services/CursoService.java
@@ -0,0 +1,13 @@
+package com.jchalan.springcloud.msvc.cursos.services;
+
+import com.jchalan.springcloud.msvc.cursos.models.entity.Curso;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CursoService {
+    List<Curso> listar();
+    Optional<Curso> porId(Long id);
+    Curso guardar(Curso guardar);
+    void eliminar(Long id);
+}
