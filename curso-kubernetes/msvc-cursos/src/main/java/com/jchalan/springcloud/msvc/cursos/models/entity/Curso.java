fe251646ea6c3d8fcf4c6cbf2ebf399078bc5b50
Francisco Chalan <francischalan@gmail.com>
2025-04-22 07:53:03 -0500

    feat: add CursoUsuarios entity

--- curso-kubernetes/msvc-cursos/src/main/java/com/jchalan/springcloud/msvc/cursos/models/entity/Curso.java
+++ curso-kubernetes/msvc-cursos/src/main/java/com/jchalan/springcloud/msvc/cursos/models/entity/Curso.java
@@ -3,6 +3,9 @@ package com.jchalan.springcloud.msvc.cursos.models.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "cursos")
 public class Curso {
@@ -13,6 +16,13 @@ public class Curso {
 
     @NotEmpty(message = "El nombre no puede estar vacío")
     private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CursoUsuario> cursoUsuarios;
+
+    public Curso() {
+        cursoUsuarios = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -29,4 +39,21 @@ public class Curso {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public void addCursoUsuario(CursoUsuario cursoUsuario) {
+        this.cursoUsuarios.add(cursoUsuario);
+    }
+
+    public void removeCursoUsuario(CursoUsuario cursoUsuario) {
+        this.cursoUsuarios.remove(cursoUsuario);
+    }
+
+    public List<CursoUsuario> getCursoUsuarios() {
+        return cursoUsuarios;
+    }
+
+    public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
+        this.cursoUsuarios = cursoUsuarios;
+    }
+
 }
